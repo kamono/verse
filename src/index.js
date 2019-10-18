b03449cd38737a06be7985bfb207c271ffb40a44
nate kamono <nate@plastiq.com>
2019-10-18 07:28:06 -0700

    Added function, which returns a random string.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('asfd');
+
+const giveMeARandomString = (length) => {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+module.exports = giveMeARandomString;
